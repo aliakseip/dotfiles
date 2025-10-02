740e4247e1edf27fbed5b5f523536bc7f8d31f52
Aliaksei Pashkouski <apashkouski@splunk.com>
2025-10-02 18:27:30 +0200

    Adds listchars characters

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -10,3 +10,13 @@ vim.opt.shiftwidth = 4
 vim.opt.cursorline = true
 vim.opt.laststatus = 3
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = "» ",
+    trail = "•",
+    eol = "↲",
+    nbsp = "␣",
+    extends = "›",
+    precedes = "‹",
+}
